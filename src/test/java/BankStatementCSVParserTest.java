e0931622de03349ca66d3c863a4a643bc5c701ef
mollen <stepanpanenko@gmail.com>
2025-11-26 11:39:17 +0300

    BankStatementCSVParserTest.java and BankStatementJSONParserTest.java

--- src/test/java/BankStatementCSVParserTest.java
+++ src/test/java/BankStatementCSVParserTest.java
@@ -4,26 +4,24 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Month;
 
-public class BankStatementJSONParserTest
+public class BankStatementCSVParserTest
 {
-    BankStatementParser bankStatementJSONParser = new BankStatementJSONParser();
+    BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
     @Test
     public void shouldParseOneCorrectLine()
     {
-        String line = "{\n" +
-                "  \"description\": \"Salary\",\n" +
-                "  \"date\": \"18-08-2020\",\n" +
-                "  \"amount\": 190000}";
+        String line = "Salary,18-08-2020,190000";
+
         BankTransaction expected = new BankTransaction(
                 "Salary",
                 LocalDate.of(2020, Month.AUGUST, 18),
                 190000);
 
-        BankTransaction result = bankStatementJSONParser.parseFrom(line);
+        BankTransaction result = bankStatementParser.parseFrom(line);
 
-        double delta = 0.0d;
-        Assert.assertEquals(expected.getAmount(), result.getAmount(), delta);
         Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
     }
 }
