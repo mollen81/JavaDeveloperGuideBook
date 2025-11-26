a7a239a4867dc558e151bc3054847d4476a3f405
mollen <stepanpanenko@gmail.com>
2025-11-26 11:50:07 +0300

    BankStatementCSVParserTest.java test for parseLines

--- src/test/java/BankStatementCSVParserTest.java
+++ src/test/java/BankStatementCSVParserTest.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BankStatementCSVParserTest
 {
@@ -23,5 +25,26 @@ public class BankStatementCSVParserTest
         Assert.assertEquals(expected.getDate(), result.getDate());
         Assert.assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldParseCorrectLines()
+    {
+        List<String> lines = new ArrayList<>();
+        lines.add("Salary,18-08-2020,190000");
+        lines.add("Salary,20-08-2020,200000");
+
+        List<BankTransaction> expected = new ArrayList<>();
+        expected.add(new BankTransaction("Salary", LocalDate.of(2020, Month.AUGUST, 18), 190000));
+        expected.add(new BankTransaction("Salary", LocalDate.of(2020, Month.AUGUST, 20), 200000));
+
+        List<BankTransaction> result = bankStatementParser.parseLinesFrom(lines);
+
+        for(int i = 0; i < result.size(); i++)
+        {
+            Assert.assertEquals(result.get(i).getDescription(), expected.get(i).getDescription());
+            Assert.assertEquals(result.get(i).getAmount(), expected.get(i).getAmount(), 0.0d);
+            Assert.assertEquals(result.get(i).getDate(), expected.get(i).getDate());
+        }
     }
 }
